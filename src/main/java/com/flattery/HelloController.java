27d2331533d370dc9d17d7db71dfe10ef26de660
Christian Werner <cw@rizzistrasse.com>
2019-05-03 20:02:12 +0200

    init Commit

---
+++ src/main/java/com/flattery/HelloController.java
@@ -0,0 +1,20 @@
+package com.flattery;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "TestPage";
+    }
+
+
+}
---
+++ src/test/java/com/flattery/HelloFlatteryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flattery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloFlatteryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
