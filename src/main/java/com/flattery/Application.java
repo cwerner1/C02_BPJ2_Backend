3bed3337a88f3e71691d23e70d64202b25e76378
Christian Werner <cw@rizzistrasse.com>
2019-06-17 08:36:55 +0200

    Mysql Connector - missing EntityFactoryManager

--- src/main/java/com/flattery/Application.java
+++ src/main/java/com/flattery/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.flattery.models")
-
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/flattery/models/WohnungRepository.java
+++ src/main/java/com/flattery/models/WohnungRepository.java
@@ -1,12 +1,12 @@
 package com.flattery.models;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
 
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
-
+@Repository
 public interface WohnungRepository extends CrudRepository<Wohnung, Integer> {
 
 }
