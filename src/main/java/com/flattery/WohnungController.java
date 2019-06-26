093c4c4019e379c0b62faa2bb54a7cd575449d5f
Christian Werner <cw@rizzistrasse.com>
2019-06-26 17:37:17 +0200

    adding Entry

--- src/main/java/com/flattery/WohnungController.java
+++ src/main/java/com/flattery/WohnungController.java
@@ -45,9 +45,9 @@ public class WohnungController extends BaseController{
         return "Saved";
     }
 
-    @GetMapping(path = "/add") // Map ONLY GET Requests
+    @PostMapping(path = "/add") // Map ONLY GET Requests
     public @ResponseBody
-    String addNewWohnung(@RequestParam String payload) {
+    String addNewWohnung(@RequestBody String payload) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
